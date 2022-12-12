d60af1eaa790cfe6fb6dcf98d6b49e55fb14f99b
Leonel <leonelrivero@tdf.edu.ar>
2022-12-12 18:46:44 -0300

    arreglada API(Funciono :D)

--- 2022-11-23/Consumo API/script.js
+++ 2022-11-23/Consumo API/script.js
@@ -1,23 +1,26 @@
 "use strict"
 
-let contenedor = document.getElementsByID("container");
+let contenedor = document.getElementById('container');
 
-fetch('https://api.chucknorris.io/jokes/random')
+fetch('https://www.thesportsdb.com/api/v1/json/2/all_sports.php')
  .then(response => response.json())
  .then(data => {
-  console.log(`Nombre: ${data.categories} Apellido: ${data.categories.id} Titulo: ${data.categories.icon_url}`)
-         const e = data;
-      
-      let h2 = document.createElement('h2');
-      let h3 = document.createElement('h3');
-      let p = document.createElement('p');
-      let img = document.createElement('img');
-      h2.innerText = e.results.categories;
-      h3.innerText = e.results.id;
-      p.innerText = e.results.value;
-      img.src = e.results.icon_url;
-      contenedor.appendChild(h2);
-      contenedor.appendChild(h3);
-      contenedor.appendChild(p);
-      contenedor.appendChild(img);
-    });
+  console.log(`ID: ${data.sports[0].idSport} Deporte: ${data.sports[0].strSport} Formato: ${data.sports[0].strFormat}`)
+  for (let index = 0; index < data.sports.length; index++) {
+     const element = data.sports[index];
+     
+     
+     let h2 = document.createElement('h2');
+     let h3 = document.createElement('h3');
+     let p = document.createElement('p');
+     let img = document.createElement('img');
+     h2.innerText = element.idSport;
+     h3.innerText = element.strSport;
+     p.innerText = element.strFormat;
+     img.src = element.strSportThumb;
+     contenedor.appendChild(h2);
+     contenedor.appendChild(h3);
+     contenedor.appendChild(p);
+     contenedor.appendChild(img);
+  }
+});  
